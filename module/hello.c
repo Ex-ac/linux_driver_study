f002597227ae01d6b3a2e0ef427b372768ab33a8
Ex-ac <ex-ac@outlook.com>
2019-03-24 19:15:39 +0800

    第4章结束

--- module/hello.c
+++ module/hello.c
@@ -3,9 +3,13 @@
 #include <linux/module.h>
 #include <linux/err.h>
 
-static void __init hello_init(void)
+static char *author = "Ex-ac"; 
+module_param(author, charp, S_IRUGO);
+
+static int __init hello_init(void)
 {
-    printfk(KERN_INFO "Hello world enter.\n");
+    printk(KERN_INFO "Hello world enter.\n");
+    printk(KERN_INFO "Author %s\n", author);
     return 0;
 }
 
@@ -13,10 +17,23 @@ module_init(hello_init);
 
 static void __exit hello_exit(void)
 {
-    printfk(KERN_INFO "Hello world exit.\n");
+    printk(KERN_INFO "Hello world exit.\n");
 }
 
 module_exit(hello_exit);
+
+
+static int add_integar(int a, int b)
+{
+    return a + b;
+}
+static int sub_integar(int a, int b)
+{
+    return a - b;
+}
+
+EXPORT_SYMBOL_GPL(add_integar);
+EXPORT_SYMBOL_GPL(sub_integar);
 
 MODULE_AUTHOR("Ex-ac@outlook.com");
 MODULE_LICENSE("GPL");
