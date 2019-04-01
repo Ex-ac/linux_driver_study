e158b49ba6892b10eb29d02e51ff8dc8c4eba4d5
Ex-ac <ex-ac@outlook.com>
2019-04-01 12:57:34 +0800

    2019/04/01 12:57

---
+++ globalmem/globalmem_mutex.c
@@ -0,0 +1,10 @@
+#include <linux/mutex.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/uaccess.h>
+
