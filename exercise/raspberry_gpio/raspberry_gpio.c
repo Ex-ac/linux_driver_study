b4855bcd2e7575fca441b86bd8856655ce027b62
Ex-ac <ex-ac@outlook.com>
2019-05-15 23:27:38 +0800

    2019/05/15 23:27

--- exercise/raspberry_gpio/raspberry_gpio.c
+++ exercise/raspberry_gpio/raspberry_gpio.c
@@ -21,23 +21,316 @@
 #include <asm-generic/io.h>
 
 
+static int rasp_gpio_major = 0;
+module_param(rasp_gpio_major, int, S_IRUGO);
+
+static int rasp_gpio_size = RASP_GPIO_SIZE;
+module_param(rasp_gpio_size, int, S_IRUGO);
+
+static uint rasp_gpio_phys_addr = 0x00;
+module_param(rasp_gpio_phys_addr, uint, S_IRUGO);
+
+struct rasp_gpio_pin
+{
+    bool detect_enable;
+    bool pull_enable;
+    uint8_t detect_type,;
+    uint8_t status;
+    uint8_t io_type;
+};
+
+static inline void rasp_gpio_pin_init(struct rasp_gpio_pin *gpio_pin)
+{
+    gpio_pin->detect_enable = false;
+    gpio_pin->detect_type = RASP_GPIO_DETECT_TYPE_NONE;
+    gpio_pin->pull_enable = false;
+    gpio_pin->status = RASP_GPIO_STATUS_UNAVAILABLE;
+    gpio_pin->io_type = GPIO_IO_TYPE_DEFAULT,
+}
+
+struct rasp_gpio_dev
+{
+    uint8_t pull_type;
+    struct rasp_gpio_pin *pins;
+    struct cdev cdev;
+};
+
+void __iomem *gpio_base = NULL;
+struct rasp_gpio_dev *rasp_gpio_devp;
 
 
-void __iomem *gpio_base;
-
-
-
-
-
-
-#include <stdio.h>
-
-int main()
-{   
-    int i = 0;
-    for (; i < 6; ++i)
+inline int gpio_set_io_type(uint8_t index, uint8_t iotype)
+{
+    void __iomem *addr = gpio_base + ((uint32_t)(GPIO_FSEL_BASE - GPIO_BASE) + 4 * (index / 10)) / 4;
+    if (iotype > 0x07)
     {
-        printf("GPFSEL%i: 0x%x\n", i, GPIO_FSEL(i));
+        return -EINVAL;
     }
+    uint32_t temp = iotype << (3 * (index % 3));
+    iowrite32(temp, addr);
     return 0;
+}
+
+inline uint8_t gpio_get_io_type(uint8_t index)
+{
+    void __iomem *addr = gpio_base + ((uint32_t)(GPIO_FSEL_BASE - GPIO_BASE) + 4 * (index / 10)) / 4;
+    volatile uint32_t temp = ioread32(addr);
+    return temp >> (3 * (index % 3));
+}
+
+inline void _gpio_set_bits(uint32_t pins, uint32_t offset)
+{
+    void __iomem *addr = gpio_base + (uint32_t)(offset - GPIO_BASE) / 4;
+    iowrite32(pins, offset);
+}
+
+inline uint32_t _gpio_get_bits(uint32_t offset)
+{
+    void __iomem *addr = gpio_base + (uint32_t)(offset - GPIO_BASE) / 4;
+    volatile uint32_t ret = ioread32(addr);
+    return ret;
+}
+
+inline void gpio_set_bits_hight(uint32_t pins)
+{
+    _gpio_set_bits(pins & 0x3fffff, GPIO_SET_BASE + 4);
+}
+
+inline void _gpio_set_bits_low(uint32_t pins)
+{
+    _gpio_set_bits(pins, GPIO_SET_BASE);
+}
+
+#define gpio_set_bit(index) \
+do  \
+{   \
+    if (index > 32) \
+    {   \
+        gpio_set_bits_hight(GPIO_PIN_(index));  \
+    }   \
+    else    \
+    {   \
+        gpio_set_bits_low(GPIO_PIN_(index));    \
+    }   \
+} while(0)
+
+
+inline void gpio_clear_bits_hight(uint32_t pins)
+{
+    _gpio_set_bits(pins & 0x3fffff, GPIO_CLEAR_BASE + 4);
+}
+
+inline void gpio_clear_bits_low(uint32_t pins)
+{
+    _gpio_set_bits(pins, GPIO_CLEAR_BASE);
+}
+
+#define gpio_clear_bit(index) \
+do  \
+{   \
+    if ((index) > 32)   \
+    {   \
+        gpio_set_bits_hight(GPIO_PIN_(index));  \
+    }   \
+    else    \
+    {   \
+        gpio_set_bits_low(GPIO_PIN_(index));    \
+    }   \
+} while (0)
+
+inline uint32_t gpio_levels_hight(void)
+{
+    return _gpio_get_bits(GPIO_LEV_BASE + 4);
+}
+
+inline uint32_t gpio_levels_low(void)
+{
+    return _gpio_get_bits(GPIO_LEV_BASE);
+}
+
+#define gpio_level_is_hight(index)  \
+index > 32 ? \
+    (gpio_levels_hight() & GPIO_PIN_(index) > 0 ? true : false) :   \
+    (gpio_levels_low() & GPIO_PIN_(index) > 0 ? true : false)   \
+
+#define _gpio_detect_enable(index, y, offset) \
+do  \
+{   \
+    void __iomem *addr = gpio_base + (offset) - GPIO_BASE + ((index) > 32 ? 4 : 0) / 4; \
+    volatile uint32_t temp = ioread32(addr);    \
+    if (y)  \
+    {   \
+       temp |= GPIO_PIN_(index);   \
+    }   \
+    else    \
+    {   \
+       temp &= (!GPIO_PIN_(index));    \
+    }   \
+    iowrite32(temp, addr);  \
+} while (0)
+
+
+inline void gpio_detect_enable(uint8_t index, bool y)
+{
+    _gpio_detect_enable(index, y, GPIO_EDS_BASE);
+}
+
+#define _gpio_detect_status(index, offset)  \
+((_gpio_get_bits((offset) + ((index) > 32 ? 4 : 0)) & GPIO_PIN_(index)) > 0 ? true : false)
+
+inline bool gpio_detect_status(uint8_t index)
+{
+    return _gpio_detect_status(index);
+}
+
+inline void gpio_detect_rising_enable(uint8_t index, bool y)
+{
+    _gpio_detect_enable(index, y, GPIO_REN_BASE);
+}
+
+inline bool gpio_detect_rising_status(uint8_t index)
+{
+    return _gpio_detect_status(index, GPIO_REN_BASE);
+}
+
+inline void gpio_detect_falling_enable(uint8_t index, bool y)
+{
+    _gpio_detect_enable(index, y, GPIO_REN_BASE);
+}
+
+inline bool gpio_detect_falling_status(uint8_t index)
+{
+    return _gpio_detect_status(index, GPIO_REN_BASE);
+}
+
+inline void gpio_detect_hight_enable(uint8_t index, bool y)
+{
+    _gpio_detect_enable(index, y, GPIO_HEN_BASE);
+}
+
+inline bool gpio_detect_hight_status(uint8_t index)
+{
+    return _gpio_detect_status(index, GPIO_HEN_BASE);
+}
+
+inline void gpio_detect_low_enable(uint8_t index, bool y)
+{
+    _gpio_detect_enable(index, y, GPIO_LDN_BASE);
+}
+
+inline bool gpio_detect_low_statu(uint8_t index)
+{
+    return _gpio_detect_status(index, GPIO_LDN_BASE);
+}
+
+inline void gpio_detect_async_rising_enable(uint8_t index, bool y)
+{
+    _gpio_detect_enable(index, y, GPIO_AREN_BASE);
+}
+
+inline bool gpio_detect_async_rising_status(uint8_t index, bool y)
+{
+    return _gpio_detect_status(index, y, GPIO_AREN_BASE);
+}
+
+inline void gpio_dected_async_falling_enable(uint8_t index, bool y)
+{
+    _gpio_detect_enable(index, y, GPIO_AFEN_BASE);
+}
+
+inline bool gpio_dected_async_falling_enable(uint8_t index)
+{
+    return _gpio_detect_status(index, GPIO_AFEN_BASE);
+}
+
+inline void gpio_set_pull_type(enum GPIO_PULL_TYPE pull_type)
+{
+    _gpio_set_bits(pull_type & 0x07, GPIO_PULL_BASE)
+}
+
+inline enum GPIO_PULL_TYPE gpio_get_pull_type(void)
+{
+    (enum GPIO_PULL_TYPE)(_gpio_get_bits(GPIO_PULL_BASE));
+}
+
+#define _gpio_pull_enable(index, y, offset) _gpio_detect_enable(index, y, offset)
+
+#define _gpio_pull_status(index, y, offset) _gpio_detect_status(index, y, offset)
+
+inline void gpio_pull_enable(uint8_t index, bool y)
+{
+    _gpio_pull_enable(index, y, GPIO_PULL_CLK_BASE);
+}
+
+inline bool gpio_pull_status(uint8_t index)
+{
+    return _gpio_pull_status(index, GPIO_PULL_CLK_BASE);
+}
+
+static int rasp_gpio_open(struct inode *inode, struct file *filp)
+{
+    nonseekable_open(inode, filp);
+    return 0;
+}
+
+static void rasp_gpio_release(struct inode *inode, struct file *filp)
+{
+    
+}
+
+static ssize_t rasp_gpio_read(struct file *filp, char __user *buf, size_t count, loff_t *pos)
+{
+    return 0;
+}
+
+static ssize_t rasp_gpio_write(struct file *filp, const char __user *buf, size_t count, loff_t *pos)
+{
+    return 0;
+}
+
+static long rasp_gpio_ioctl(struct file *filp, unsigned int cmd, unsigned long args)
+{
+    long ret;
+    uint8_t index, value;
+
+    if (_IOC_TYPE(cmd) != RASP_GPIO_IOC_MAGIC)
+    {
+        return -EINVAL;
+    }
+
+    if (_IOC_NR(cmd) > RASP_GPIO_IOC_MAX)
+    {
+        return -EINVAL;
+    }
+    index = args & 0xff;
+    value = (args >> 8) & 0xff;
+
+    if (index > rasp_gpio_size)
+    {
+        return -EINVAL;
+    }
+
+    if (rasp_gpio_devp->pins[index]->status != RASP_GPIO_STATUS_USEING)
+    {
+        return -EFAULT;
+    }
+
+    switch (cmd)
+    {
+    case RASP_GPIO_IOC_SET_IO_TYPE:
+        ret = gpio_set_io_type(index, value);
+        if (ret == 0)
+        {
+            rasp_gpio_devp->pins[index]
+        }
+        break;
+    
+    case RASP_GPIO_IOC_GET_IO_TYPE:
+        ret = gpio_get_io_type(index);
+        break;
+
+    case RASP_GPIO_IOC_SET_B
+    default:
+        break;
+    }
 }
--- exercise/raspberry_gpio/raspberry_gpio.h
+++ exercise/raspberry_gpio/raspberry_gpio.h
@@ -1,97 +1,116 @@
 #ifndef RASPBERRY_GPIO_H_
 #define RASPBERRY_GPIO_H_
 
-#include <stdint.h>
-#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/fcntl.h>
+#include <linux/types.h>
 
-#define GPIO_BASE           0x00200000  // base offset
-#define GPIO_FSEL_BASE      0x00200000  // function setting
-#define GPIO_SET_BASE       0x0020001C  // set bit
-#define GPIO_CLEAR_BASE     0x00200028  // clear bit
-#define GPIO_LEV_BASE       0x00200034  // return the actuak value of the pin
-#define GPIO_EDS_BASE       0x00200040  // event detect status
-#define GPIO_REN_BASE       0x0020004C  // rising edge detect enable
-#define GPIO_FEN_BASE       0x00200058  // failing edge detect enable
-#define GPIO_HEN_BASE       0x00200064  // hight detect enable
-#define GPIO_LDN_BASE       0x00200070  // low detect enable
-#define GPIO_AREN_BASE      0x0020007C  // asynchronous rising edge detect enable (it use to detected very short rising edges)
-#define GPIO_AFEN_BASE      0x00200088  // asynchronous failling edge detect enable (it use to detected very short faillinfg edges)
-#define GPIO_UD_BASE        0x00200094  // pull-up/down 150 colck
-#define GPIO_UD_CLK_BASE    0x00200098  // pull-up/dowm enable 150clock
+#define GPIO_BASE 0x00200000          // base offset
+#define GPIO_FSEL_BASE 0x00200000     // function setting
+#define GPIO_SET_BASE 0x0020001C      // set bit
+#define GPIO_CLEAR_BASE 0x00200028    // clear bit
+#define GPIO_LEV_BASE 0x00200034      // return the actuak value of the pin
+#define GPIO_EDS_BASE 0x00200040      // event detect status
+#define GPIO_REN_BASE 0x0020004C      // rising edge detect enable
+#define GPIO_FEN_BASE 0x00200058      // failing edge detect enable
+#define GPIO_HEN_BASE 0x00200064      // hight detect enable
+#define GPIO_LDN_BASE 0x00200070      // low detect enable
+#define GPIO_AREN_BASE 0x0020007C     // asynchronous rising edge detect enable (it use to detected very short rising edges)
+#define GPIO_AFEN_BASE 0x00200088     // asynchronous failling edge detect enable (it use to detected very short faillinfg edges)
+#define GPIO_PULL_BASE 0x00200094     // pull-up/down 150 colck
+#define GPIO_PULL_CLK_BASE 0x00200098 // pull-up/dowm enable 150clock
+#define GPIO_BASE_LAST 0x002000B0
 
-#define GPIO_PIN_(x)        (uint32_t)(0x01 << ((x) % 32))
+#define GPIO_PIN_(x) (uint32_t)(0x01 << ((x) % 32))
 
+#undef PRINT_INFO
+#ifdef __KERNEL___
+#define PRINT_INFO(fmt, args...) printk(KERN_INFO "raspberry gpio: " fmt, ##args)
+#elif
+#define PRINT_INFO(fmt, args...) printf(std)
+#endif
 
-enum GPIO_IO_TYPE
+#define RASP_GPIO_SIZE      53
+typedef enum
 {
-    GPIO_IO_TYPE_DEFAULT    = 0x00,
-    GPIO_IO_TYPE_IN         = 0x00,
-    GPIO_IO_TYPE_OUT        = 0x01,
-    GPIO_IO_TYPE_FN0        = 0x04,
-    GPIO_IO_TYPE_FN1        = 0x05,
-    GPIO_IO_TYPE_FN2        = 0x06,
-    GPIO_IO_TYPE_FN3        = 0x07,
-    GPIO_IO_TYPE_FN4        = 0x03,
-    GPIO_IO_TYPE_FN5        = 0x02,
-};
-
-enum GPIO_PULL_TYPE
+    GPIO_STATUS_UNUSED = 0x00,
+    GPIO_STATUS_USEING,
+    GPIO_STATUS_UNAVAILABLE,
+} GPIO_STATUS;
+typedef enum
 {
-    GPIO_PULL_TYPE_NO       = 0x00,
-    GPIO_PULL_TYPE_DOWN     = 0x01,
-    GPIO_PULL_TYPE_UP       = 0x02,
-};
+    GPIO_IO_TYPE_DEFAULT = 0x00,
+    GPIO_IO_TYPE_IN = 0x00,
+    GPIO_IO_TYPE_OUT = 0x01,
+    GPIO_IO_TYPE_FN0 = 0x04,
+    GPIO_IO_TYPE_FN1 = 0x05,
+    GPIO_IO_TYPE_FN2 = 0x06,
+    GPIO_IO_TYPE_FN3 = 0x07,
+    GPIO_IO_TYPE_FN4 = 0x03,
+    GPIO_IO_TYPE_FN5 = 0x02,
+} GPIO_IO_TYPE;
+
+typedef enum
+{
+    GPIO_PULL_TYPE_NO = 0x00,
+    GPIO_PULL_TYPE_DOWN = 0x01,
+    GPIO_PULL_TYPE_UP = 0x02,
+} GPIO_PULL_TYPE;
+
+typedef enum
+{
+    GPIO_DETECT_TYPE_NONE = 0x00,
+    GPIO_DETECT_TYPE_RASING = 0x01 << 1,
+    GPIO_DETECT_TYPE_FALLING = 0x01 << 2,
+    GPIO_DETECT_TYPE_HIGHT = 0x01 << 3,
+    GPIO_DETECT_TYPE_LOW = 0x01 << 4,
+    GPIO_DETECT_TYPE_ASYNC_RASING = 0x01 << 5,
+    GPIO_DETECT_TYPE_ASYNC_FALLING = 0x01 << 6,
+} GPIO_DETECT_TYPE;
 
 extern void __iomem *gpio_base;
 
-inline void gpio_set_io_type(uint8_t index, enum GPIO_IO_TYPE iotype)
-{
-    void __iomem *addr = gpio_base + ((uint32_t)(GPIO_FSEL_BASE - GPIO_BASE) + 4 * (index / 10)) / 4;
-    uint32_t temp = iotype << (3 * (index % 3));
-    iowrite32(temp, addr);
-}
+#define RASP_GPIO_IOC_ARGS(index, value) (unsigned long)(value << 8 | index)
 
-inline void gpio_set_bits_hight(uint32_t pins)
-{
-    void __iomem *addr = gpio_base + ((uint32_t)(GPIO_SET_BASE - GPIO_BASE) + 4) / 4;
-    iowrite32(pins & 0x3ffff, addr);
-}
+#define RASP_GPIO_IOC_MAGIC 'G'
 
-inline void gpio_set_bits_low(uint32_t pins)
-{
-    void __iomem *addr = gpio_base + (uint32_t)(GPIO_SET_BASE - GPIO_BASE) / 4;
-    iowrite32(pins, addr);
-}
+#define RASP_GPIO_IOC_SET_IO_TYPE _IO(RASP_GPIO_IOC_MAGIC, 1)
+#define RASP_GPIO_IOC_GET_IO_TYPE _IO(RASP_GPIO_IOC_MAGIC, 2)
 
-inline void gpio_set_bit(uint8_t index)
-{
-    void __iomem *addr = gpio_base + ((uint32_t)(GPIO_SET_BASE - GPIO_BASE) + index > 31 ? 4 : 0) / 4;
-    iowrite32(GPIO_PIN_(index), addr);
-}
+#define RASP_GPIO_IOC_SET_DETECT _IO(RASP_GPIO_IOC_MAGIC, 3)
+#define RASP_GPIO_IOC_GET_IO_TYPE _IO(RASP_GPIO_IOC_MAGIC, 4)
 
-inline void gpio_set_bits_hight(uint32_t pins)
-{
-    void __iomem *addr = gpio_base + ((uint32_t)(GPIO_SET_BASE - GPIO_BASE) + 4) / 4;
-    iowrite32(pins & 0x3ffff, addr);
-}
+#define RASP_GPIO_IOC_SET_RISING _IO(RASP_GPIO_IOC_MAGIC, 5)
+#define RASP_GPIO_IOC_GET_RISING _IO(RASP_GPIO_IOC_MAGIC, 6)
 
-inline void gpio_set_bits_low(uint32_t pins)
-{
-    void __iomem *addr = gpio_base + (uint32_t)(GPIO_SET_BASE - GPIO_BASE) / 4;
-    iowrite32(pins, addr);
-}
+#define RASP_GPIO_IOC_SET_FALLING _IO(RASP_GPIO_IOC_MAGIC, 7)
+#define RASP_GPIO_IOC_GET_FALLING _IO(RASP_GPIO_IOC_MAGIC, 8)
 
-inline void gpio_set_bit(uint8_t index)
-{
-    void __iomem *addr = gpio_base + ((uint32_t)(GPIO_SET_BASE - GPIO_BASE) + index > 31 ? 4 : 0) / 4;
-    iowrite32(GPIO_PIN_(index), addr);
-}
+#define RASP_GPIO_IOC_SET_HIGHT _IOW(RASP_GPIO_IOC_MAGIC, 9)
+#define RASP_GPIO_IOC_GET_HIGHT _IO(RASP_GPIO_IOC_MAGIC, 10)
 
+#define RASP_GPIO_IOC_SET_LOW _IOW(RASP_GPIO_IOC_MAGIC, 11)
+#define RASP_GPIO_IOC_GET_LOW _IO(RASP_GPIO_IOC_MAGIC, 12)
 
-inline void gpio_set_bits(uint32_t pins, uint32_t offset)
-{
-    void __iomem *addr
-}
+#define RASP_GPIO_IOC_SET_A_RISING _IOW(RASP_GPIO_IOC_MAGIC, 13)
+#define RASP_GPIO_IOC_GET_A_RISING _IO(RASP_GPIO_IOC_MAGIC, 14)
 
+#define RASP_GPIO_IOC_SET_A_FALLING _IOW(RASP_GPIO_IOC_MAGIC, 15)
+#define RASP_GPIO_IOC_GET_A_FALLING _IO(RASP_GPIO_IOC_MAGIC, 16)
+
+#define RASP_GPIO_IOC_SET_PULL_TYPE _IO(RASP_GPIO_IOC_MAGIC, 17)
+#define RASP_GPIO_IOC_GET_PULL_TYPE _IO(RASP_GPIO_IOC_MAGIC, 18)
+
+#define RASP_GPIO_IOC_SET_PULL _IOW(RASP_GPIO_IOC_MAGIC, 19)
+#define RASP_GPIO_IOC_GET_PULL _IO(RASP_GPIO_IOC_MAGIC, 20)
+
+#define RASP_GPIO_IOC_SET_BIT _IO(RASP_GPIO_IOC_MAGIC, 21)
+#define RASP_GPIO_IOC_CLEAR_BIT _IO(RASP_GPIO_IOC_MAGIC, 22)
+#define RASP_GPIO_IOC_GET_LEVEL _IO(RASP_GPIO_IOC_MAGIC), 23)
+
+#define RASP_GPIO_IOC_USE_GPIO  _IO(RASP_GPIO_IOC_MAGIC, 24)
+#define RASP_GPIO_IOC_UNUSE_GPIO _IO(RASP_GPIO_IOC_MAGIC, 25)
+
+#define RASP_GPIO_IOC_MAX   26
 
 #endif
